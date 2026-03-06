156ca82bb83f5cf89eeeae638076844c801ed709
绿毛龟 <jy33gy@163.com>
2026-03-06 02:22:46 +0000

    .

---
+++ RescaleText/rescale.js
@@ -0,0 +1,13 @@
+
+export class RescaleText {
+      constructor() {
+        var sizeLabel = "rescaleable";
+        var min = 10;
+        var max = 40;
+        var step = 1;
+        var current = 20;
+      }
+
+
+    
+    }
