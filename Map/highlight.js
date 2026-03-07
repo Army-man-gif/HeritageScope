8606f8c2be6d8d2c36b73a934ae4e1dc26ab3874
绿毛龟 <jy33gy@163.com>
2026-03-07 03:43:57 +0000

    .

---
+++ Map/highlight.js
@@ -0,0 +1,48 @@
+export class Highlight {
+  constructor(map) {
+    this.map = map;
+    this.highlightLayer = L.layerGroup().addTo(map);
+
+    /* layerTable = {
+    "lid1": polygonObj,
+    "lid2": polygonObj
+    } */
+    this.layerTable = {};
+  }
+
+  /* locationID -> polygon */
+  lid2poly(lid){
+    return void 0;
+  }
+
+  add(lid){
+    const poly = this.lid2poly(lid);
+    if (poly) {
+        this.highlightLayer.addLayer(poly);
+        this.layerTable[lid] = poly;
+    }
+  }
+
+  remove(lid){
+    const poly = this.layerTable[lid];
+    if (poly) {
+        this.highlightLayer.removeLayer(poly);
+        delete this.layerTable[lid];
+    }
+  }
+
+  style(lid, style){
+    const poly = this.layerTable[lid];
+    if (poly) {
+        poly.setStyle(style);
+    }
+    else {
+        console.warn(`No polygon found for location ID: ${lid}`);
+    }
+  }
+
+  clearHighlight(){
+    this.highlightLayer.clearLayers();
+    this.layerTable = {};
+  }
+}
