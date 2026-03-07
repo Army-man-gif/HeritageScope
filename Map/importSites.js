bdd9e70e07dcb10b6e628bad682bdab706583c51
Armaan Khaitan <axk1545@student.bham.ac.uk>
2026-03-07 00:02:44 +0000

    "Added dynamic language selection and modification. User's location auto detected and can select on the cultural sites and shows popups of information about the name and a short description"

--- Map/importSites.js
+++ Map/importSites.js
@@ -1,5 +1,5 @@
 import {createMap} from './createMap.js';
-
+let currentLanguage = "en";
 // Load GeoJSON file
 // Now I can import whole file stuff. Instead I will try to add specific live sites
 try{
@@ -7,6 +7,40 @@ try{
     const jsonData = await fetchData.json();
     console.log("Successfully imported file data and json parsed it");
     const map = createMap();
+    const initialFeature  = jsonData.features[0];
+    const languages = Object.keys(initialFeature.properties)
+        .filter(key => key.startsWith("name_"))
+        .map(key => key.slice("name_".length));
+
+    const converter = new Intl.DisplayNames(['en'], { type: 'language' });
+    const codeValuePairs = languages.map(code => ({
+        code,
+        label : converter.of(code)
+    }));
+    // Now I have all the lanauges and their display versions as well
+
+    function dynamicallyBuildLanguageSelection(){
+        const optionBox = L.DomUtil.create('div','container');
+        L.DomEvent.disableClickPropagation(optionBox);
+        codeValuePairs.forEach(pair => {
+            const label = L.DomUtil.create('label','Langlabel',optionBox);
+            label.append(`${pair.label }`)
+            const select =  L.DomUtil.create('input','selection',label);
+            select.type  = "radio";
+            select.name = "language";
+            select.value = pair.code;
+
+            if(pair.code === currentLanguage){
+                select.checked = true;
+            }
+            select.addEventListener('change',function(){
+                updateLanguage(select.value);
+            });
+            
+        })
+        return optionBox;
+
+    }
 
     // There's a lot of points so I will use marker cluster to speed things up
 
@@ -14,48 +48,42 @@ try{
     function convPointToLayer(feature,latlng){
         return L.marker(latlng);
     }
+    function updateLanguage(lang){
+        currentLanguage = lang;
 
+        markers.eachLayer(layer => {
+            if(layer.feature){
+                layer.setPopupContent(buildLanguageSpecificPopup(layer.feature));
+            }
+        });
+
+    }
+    function buildLanguageSpecificPopup(feature){
+        if(feature.properties){
+            const langKey = `name_${currentLanguage}`
+            const descriptionKey = `short_description_${currentLanguage}`
+
+            const name = feature.properties[langKey] || "No available name";
+            const description = feature.properties[descriptionKey] || "No available description";
+
+            const langDisplay = new Intl.DisplayNames(['en'],{type: "language"}).of(currentLanguage);
+
+            const popupText = `
+            <b>${langDisplay} name: ${name}</b><br>
+            <b>Short ${langDisplay} description: <br><br> ${description}
+            </b><br><br>
+            `;
+            return popupText;
+        }
+    }
     function onEachFeature(feature,layer){
         if(feature.properties){
-            let popupText = '';
-            const languages = Object.keys(feature.properties)
-                .filter(key => key.startsWith('name_'));
-            const descriptions = Object.keys(feature.properties)
-                .filter(key => key.startsWith('short_description_'))
-            languages.forEach(langKey => {
-                // Start at index 5 where the acc language code starts
-                const langCode = langKey.slice("name_".length);
-                const langDisplay = new Intl.DisplayNames(['en'], { type: "language" }).of(langCode);
-                popupText += `<b>${langDisplay} name: ${feature.properties[langKey]}</b><br>`;
-            });
-            popupText += "<br>"
-            descriptions.forEach(descriptionKey => {
-                // Start at index 5 where the acc language code starts
-                const descriptionCode = descriptionKey.slice('short_description_'.length);
-                const descriptionDisplay = new Intl.DisplayNames(['en'], { type: "language" }).of(descriptionCode);
-                let description = feature.properties[descriptionKey];
-                if(!description){
-                    description = "No available description";
-                }
-                popupText += `<b>Short ${descriptionDisplay} description: <br><br> ${description}</b><br><br>`;
-            });
-            // Attach text to a popup event
-            layer.bindPopup(popupText,{
+            layer.bindPopup(buildLanguageSpecificPopup(feature), {
                 maxWidth: 300,
                 maxHeight: 200,
                 autoPan: true
             });
         }
-
-        // Customise how the popup acitvation works
-        layer.on('click',function(e){
-            if(this.isOpenPopup()){
-                this.closePopup();
-            }else{
-                this.openPopup();
-            }
-        })
-
     }
     let markers = L.markerClusterGroup();
     L.geoJson(jsonData,{
@@ -65,8 +93,14 @@ try{
 
     map.addLayer(markers);
     map.fitBounds(markers.getBounds());
-
-
+    // Extend layer control
+    const LanguageControl  = L.Control.extend({
+        onAdd: function() {
+            return dynamicallyBuildLanguageSelection();
+        }
+    });
+    const languageController = new LanguageControl({position : "topright"});
+    map.addControl(languageController);
 
 }catch (error){
     console.error("Error loading dataset file", error);
