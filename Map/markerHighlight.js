ad1252455202bc7114b93b6414a230ff465fb1eb
绿毛龟 <jy33gy@163.com>
2026-03-09 22:24:15 +0000

    remove old hightlight and merge

--- Map/markerHighlight.js
+++ Map/markerHighlight.js
@@ -69,3 +69,94 @@ export class MarkerHighlight {
         }
     }
 }
+
+export class AreaHighlight {
+  constructor(map) {
+    this.map = map;
+    this.highlightLayer = L.layerGroup().addTo(map);
+
+    /* layerTable = {
+    "lid1": polygonObj,
+    "lid2": polygonObj
+    } */
+    this.layerTable = {};
+  }
+
+  /* locationID -> polygon */
+  async fetchMockingData(path) {
+    try {
+      const response = await fetch(path);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        this.m_data = data;
+      } catch (error) {
+        console.error('Error fetching mocking data:', error);
+        this.m_data = null;
+      }
+    }
+
+
+
+/* this only read mocking data for demonstration */
+  lid2poly(lid){
+    if (!this.m_data) {
+      console.warn("Mocking data not loaded. Call fetchMockingData() first.");
+      return void 0;
+    }
+    const polyData = this.m_data[lid];
+    if (!polyData) {
+      console.warn(`No polygon data found for location ID: ${lid}`);
+      return {code: -1, message: `No polygon data for lid: ${lid}`};
+    }
+    /* const latlngs = polyData.map(coord => [coord.lat, coord.lng]); */
+    data = L.polygon(polyData, {
+      color: 'yellow',
+      fillColor: 'yellow',
+      fillOpacity: 0.6,
+      weight: 2
+    });
+
+    return {code: 0, message: "Polygon created successfully", data: data};
+  }
+
+  add(lid){
+    data = this.lid2poly(lid);
+    if (data.code !== 0) {
+      console.warn(`Failed to create polygon for lid: ${lid}. Reason: ${data.message}`);
+      return;
+    }
+
+    const poly = data.data;
+    if (this.layerTable[lid]) {
+      console.warn(`Polygon for location ID: ${lid} already exists. Skipping add.`);
+      return;
+    }
+    this.highlightLayer.addLayer(poly);
+    this.layerTable[lid] = poly;
+  }
+
+  remove(lid){
+    const poly = this.layerTable[lid];
+    if (poly) {
+        this.highlightLayer.removeLayer(poly);
+        delete this.layerTable[lid];
+    }
+  }
+
+  style(lid, style){
+    const poly = this.layerTable[lid];
+    if (poly) {
+        poly.setStyle(style);
+    }
+    else {
+        console.warn(`No polygon found for location ID: ${lid}`);
+    }
+  }
+
+  clearHighlight(){
+    this.highlightLayer.clearLayers();
+    this.layerTable = {};
+  }
+}
