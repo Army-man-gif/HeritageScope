e1eb011da57699ce8afeb1adf4f0b0ea1c476f77
Jocelyn <jxr416@student.bham.ac.uk>
2026-03-11 15:24:25 +0000

    Added some dependencies and text

--- Simulation/script.js
+++ Simulation/script.js
@@ -1,5 +1,7 @@
 //update functions
 
+// more tourists means more litter and traffic
+// more people- buildings
 function updateTourists() {
 
   // update slider text value
@@ -8,6 +10,8 @@ function updateTourists() {
   // add cars depending on input
   buildingsDiv.innerHTML = ""; // remove old buildings and people
   peopleDiv.innerHTML = "";
+  litterText.innerHTML = "";
+  trafficText.innerHTML = "";
 
   for (let i = 0; i < touristSlider.value / 20; i++) {
 
@@ -37,8 +41,27 @@ function updateTourists() {
     buildingsDiv.appendChild(building);
   }
 
+  // update litter based on tourists
+  // need litter slider to exist before running
+  if(touristSlider.value < 80){
+    litterSlider.value = touristSlider.value * 0.8;
+    updateLitter();
+    litterText.innerHTML += "As more tourists visit, more litter is found.<br><br>";
+
+    trafficSlider.value = touristSlider.value * 0.6;
+    updateCars();
+    trafficText.innerHTML += "As more tourists visit, this causes more traffic.<br><br>";
+  }
+  else{
+    litterSlider.value = touristSlider.value * 0.82;
+    updateLitter();
+    litterText.innerHTML += "As more tourists visit, more litter is found.<br><br>";
+  }
+
 }
 
+// with more litter animals can die, plants can die too
+// make brown if litter too great
 function updateLitter() {
 
   // update slider text value
@@ -47,6 +70,7 @@ function updateLitter() {
   // add cars depending on input
   litterDiv.innerHTML = ""; // remove old trash
 
+  // add trash near tourists
   for (let i = 0; i < litterSlider.value / 20; i++) {
 
     let x = Math.trunc(placedTourists[i*2]) + 50;
@@ -63,6 +87,8 @@ function updateLitter() {
     litterDiv.appendChild(litter);
   }
 
+  // above 80 add litter by building
+  // reduce trees- animals can die with lots of litter
   if(litterSlider.value > 80){
 
     for(let i=0; i<3; i++){
@@ -83,6 +109,7 @@ function updateLitter() {
 
 }
 
+// more traffic- higher temperature
 function updateCars() {
 
   // update slider text value
@@ -105,7 +132,8 @@ function updateCars() {
   }
 }
 
-
+// floods- some vegetation die
+// could be caused by global warming
 function updateRainfall() {
 
   // update slider text value
@@ -114,15 +142,58 @@ function updateRainfall() {
   // add cars depending on input
   treesDiv.innerHTML = "";
   floodsDiv.innerHTML = "";
+  rainfallText.innerHTML = "";
 
-  if(rainfallSlider.value < 30){
+  if(rainfallSlider.value < 20){
     simBackground.src = "images/brown-background.png";
+    rainfallText.innerHTML += "With low rainfall, plants die and the ground dries up. <br><br>";
   }
   else{
     simBackground.src = "images/green-background.png";
   }
 
-  if(rainfallSlider.value > 30){
+  if(rainfallSlider.value > 80){
+
+    treesDiv.innerHTML = "";
+    rainfallText.innerHTML += "Too much rainfall can cause plants to die from rotting and lack of oxygen. Floods may also occur. Climate change can cause unusual weather like long periods of heavy rain.<br><br> ";
+
+    for(let i=0; i < (rainfallSlider.value - 80)/10; i++)
+    {
+      let x = Math.trunc(placedFloods[i*2]);
+      let y = Math.trunc(placedFloods[i*2 + 1]);
+
+      let flood = document.createElement("img");
+      flood.src = "images/puddle.png";
+      flood.style.position = "absolute";
+      flood.style.bottom = y + "px";
+      flood.style.left = x + "px";
+      flood.style.width = "140px";
+      flood.style.height = "140px";
+
+      floodsDiv.appendChild(flood);
+    }
+
+    // decrease trees as rainfall is too high
+    for(let i = 0; i < (100 - rainfallSlider.value)/5 +1; i++){
+      // decrease trees
+
+      x = Math.trunc(placedTrees[i*2]);
+      y = Math.trunc(placedTrees[i*2 + 1]);
+
+      let tree = document.createElement("img");
+      tree.src = "images/tree.png";
+      tree.style.position = "absolute";
+      tree.style.bottom = y + "px";
+      tree.style.left = x + "px";
+      tree.style.width = "40px";
+      tree.style.height = "40px";
+
+      treesDiv.appendChild(tree);
+    }
+  }
+  else if(rainfallSlider.value > 20){
+
+    rainfallText.innerHTML += "With more rainfall, plants grow more. <br><br>";
 
     let treeCount = Math.min(5, Math.floor((rainfallSlider.value - 30) / 10) + 1);
 
@@ -143,38 +214,71 @@ function updateRainfall() {
     }
 
   }
-  // > 80 some trees may die, floods
-  if(rainfallSlider.value > 80){
-
-  }
 
   
 } 
 
-
+// higher temp- some trees die, less rainfall
+// could be normal or caused by traffic, or global warming
 function updateTemp() {
 
   // update slider text value
   tempVal.innerHTML = tempSlider.value;
-  
-  // add cars depending on input
-  floodsDiv.innerHTML = "";
+
+  tempText.innerHTML = "";
+
+  // update rainfall with temp
+  // low temp shouldn't affect rainfall
+
+  if(tempSlider.value > 30){
+    rainfallSlider.value = 100 - tempSlider.value + 15;
+    updateRainfall();
+
+    tempText.innerHTML += "As temperature increases, rainfall usually decreases.<br><br>";
+  }
 
   if(tempSlider.value > 80){
     simBackground.src = "images/brown-background.png";
+
+    tempText.innerHTML += "When the temperature is high the ground dries out and plants die. Climate change can cause long unusual heatwaves.<br><br>";
   }
   else{
     simBackground.src = "images/green-background.png";
   }
 
-  // in low temps some trees should die
+  // decrease trees if too cold
+  if(tempSlider.value < 20){
+
+    treesDiv.innerHTML = "";
+
+    for(let i = 0; i < (tempSlider.value)/5; i++){
+      // decrease trees
+
+      x = Math.trunc(placedTrees[i*2]);
+      y = Math.trunc(placedTrees[i*2 + 1]);
+
+      let tree = document.createElement("img");
+      tree.src = "images/tree.png";
+      tree.style.position = "absolute";
+      tree.style.bottom = y + "px";
+      tree.style.left = x + "px";
+      tree.style.width = "40px";
+      tree.style.height = "40px";
+
+      treesDiv.appendChild(tree);
+    }
+
+    tempText.innerHTML += "When the temperature is too low plants can die. With climate change the world is warming as a whole but it can also cause unusual weather patterns like cold spikes.<br><br>";
+
+  }
 
 }
 
 // x,y of tourists
 placedTourists = [30, 360, 50, 100, 200, 200, 200, 30, 300, 300];
 placedLitter = [310,140, 290, 150, 330, 150];
-placedTrees = [50, 210, 200, 360, 360, 180, 30, 30, 400, 30]
+placedTrees = [50, 210, 200, 360, 360, 180, 30, 30, 400, 30];
+placedFloods = [140, 50, 100, 240];
 
 var simBackground = document.getElementById("simBackground");
 
@@ -184,35 +288,40 @@ var trafficSlider = document.getElementById("trafficSlider");
 var trafficVal = document.getElementById("trafficVal");
 trafficVal.innerHTML = trafficSlider.value; // Display the default slider value
 const carsDiv = document.getElementById("cars");
-// initialise cars
-updateCars();
+const trafficText = document.getElementById("trafficText");
 
 var touristSlider = document.getElementById("touristSlider");
 var touristVal = document.getElementById("touristVal");
 touristVal.innerHTML = touristSlider.value; // Display the default slider value
 const buildingsDiv = document.getElementById("buildings");
 const peopleDiv = document.getElementById("people");
-// initialise tourists
-updateTourists();
+const peopleText = document.getElementById("peopleText");
 
 var litterSlider = document.getElementById("litterSlider");
 var litterVal = document.getElementById("litterVal");
 litterVal.innerHTML = litterSlider.value; // Display the default slider value
 const litterDiv = document.getElementById("litter");
-updateLitter();
+const litterText = document.getElementById("litterText");
 
 var rainfallSlider = document.getElementById("rainfallSlider");
 var rainfallVal = document.getElementById("rainfallVal");
 rainfallVal.innerHTML = rainfallSlider.value; // Display the default slider value
 const floodsDiv = document.getElementById("floods");
 const treesDiv = document.getElementById("trees");
-updateRainfall();
+const rainfallText = document.getElementById("rainfallText");
 
 var tempSlider = document.getElementById("tempSlider");
 var tempVal = document.getElementById("tempVal");
 tempVal.innerHTML = tempSlider.value;
-updateTemp();
+const tempText = document.getElementById("tempText");
 // trees said above
+
+// initialise sliders
+trafficVal.innerHTML = trafficSlider.value;
+touristVal.innerHTML = touristSlider.value;
+litterVal.innerHTML = litterSlider.value;
+rainfallVal.innerHTML = rainfallSlider.value;
+tempVal.innerHTML = tempSlider.value;
 
 // to dynamically update
 trafficSlider.addEventListener("input", updateCars);
